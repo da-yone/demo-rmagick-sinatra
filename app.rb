0b92102450d433b7fbcd7aa8fba24932ebb7aa68
hissanova <hissanova@gmail.com>
2015-06-24 17:24:39 +0900

    initial commit

---
+++ app.rb
@@ -0,0 +1,25 @@
+require 'sinatra'
+require 'rmagick'
+require 'slim'
+
+get '/' do
+  slim :index
+end
+
+post '/' do
+  message = params['message']
+  image = Magick::ImageList.new('image.png')
+  draw = Magick::Draw.new
+  draw.annotate(image, 0, 0, 50, 100, message) do
+    self.font = 'logotype.otf'
+    self.fill = '#333333'
+    self.align = Magick::LeftAlign
+    self.stroke = 'transparent'
+    self.pointsize = 30
+    self.text_antialias = true
+    self.kerning = 1
+  end
+  image.write('public/tmp.png')
+  slim :index
+end
+
